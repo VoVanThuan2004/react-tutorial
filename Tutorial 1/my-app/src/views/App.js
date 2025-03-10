5446e348857151b4fce3f8e0a4809b4bef5db643
VoVanThuan2004 <vot04766@gmail.com>
2025-03-10 22:43:25 +0700

    Update code

--- Tutorial 1/my-app/src/views/App.js
+++ Tutorial 1/my-app/src/views/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MyComponent from './Example/MyComponent';
+import ButtonClick from './Example/ButtonClick';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +17,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Tutorial  
         </a>
+
+        <b><MyComponent/></b>
+        <br></br>
+        <ButtonClick/>
       </header>
     </div>
   );
---
+++ Tutorial 1/my-app/src/views/Example/ButtonClick.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { showToast } from "./ShowToast";
+import { ToastContainer } from "react-toastify";
+
+class ButtonClick extends React.Component {
+    render() {
+        return (
+            <div>
+                <button onClick={showToast}>Click me</button>
+                <p>Congratulation for you</p>
+
+                <ToastContainer/>
+            </div>
+        )
+    }
+}
+
+export default ButtonClick
